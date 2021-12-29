1a0d3eeda921a931368b3e59fe01a7b180ce12c2
nik frank <nikbfrank@gmail.com>
2021-12-29 02:45:23 -0800

    styling for gameplay

--- src/Game.js
+++ src/Game.js
@@ -5,22 +5,34 @@ import './Game.scss';
 import { Hand } from 'react-deck-o-cards';
 
 const defHandStyle = {
-  maxHeight:'34vh',
-  minHeight:'34vh',
+  maxHeight:'32vh',
+  minHeight:'32vh',
   
   maxWidth:'100vw',
   padding: 0,
 };
 
+const pegHandStyle = {
+  maxHeight: '100%',
+  maxWidth:'100vw',
+  padding: 0,  
+};
+
 export function Game({ game: { p1: { hand: p1hand }, p2: { hand: p2hand } } }) {
 
   const [selectedCards, setSelectedCards] = useState([]);
+
+  const [playedCards, setPlayedCards] = useState([ {rank:3, suit: 3} ]);
   
   return (
     <div className="Game">
       <div className={'hand p1-hand '+(selectedCards.reduce((cl, sc)=> cl+'selected-'+sc+' ', ''))}>
         <Hand cards={p1hand} hidden={false} style={defHandStyle}
               onClick={card=> setSelectedCards(prev=> prev.includes(card) ? prev.filter(c=> c !== card) : [...prev, card])} />
+      </div>
+
+      <div className='peg'>
+        <Hand cards={playedCards} hidden={false} style={pegHandStyle} onClick={()=>0} />
       </div>
 
       <div className='hand p2-hand'>
